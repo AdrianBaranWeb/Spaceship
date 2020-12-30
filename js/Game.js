f6465edde653fcd1e0e400647f9942e95d0b345b
Adrian Baran <adrianbaranweb@gmail.com>
2020-12-30 18:26:35 +0100

    Add explosions

--- js/Game.js
+++ js/Game.js
@@ -5,12 +5,17 @@ class Game{
     #htmlElements = {
         spaceship: document.querySelector('[data-spaceship]'),
         container: document.querySelector('[data-container]'),
+        lives: document.querySelector('[data-lives]'),
+        score: document.querySelector('[data-score]'),
     }
 
     #ship = new Spaceship(this.#htmlElements.spaceship, this.#htmlElements.container)
 
     #enemies = []
     #enemiesSpeed = null
+
+    #lives = null
+    #score = null
     
     #checkPositionInterval = null
     #createEnemyInterval = null
@@ -22,37 +27,24 @@ class Game{
 
     #newGame(){
         this.#enemiesSpeed = 40
+        this.#lives = 3
+        this.#score = 0
         this.#createEnemyInterval = setInterval(() => this.#randomNewEnemy(), 1000)
         this.#checkPositionInterval = setInterval(() => this.#checkPosition(), 1)
     }
 
     #randomNewEnemy(){
         const randomNumber = Math.floor(Math.random() * 5) + 1
-        randomNumber % 5 ? this.#createNewEnemy('enemy', null, this.#enemiesSpeed) : this.#createNewEnemy('enemy--big', 3, this.#enemiesSpeed * 2)
+        randomNumber % 5 ? this.#createNewEnemy('enemy', 1, this.#enemiesSpeed, 'explosion') : this.#createNewEnemy('enemy--big', 3, this.#enemiesSpeed * 2, 'explosion--big')
     }
 
-    #createNewEnemy(enemyType, enemyLives, enemySpeed){
-        const enemy = new Enemy(this.#htmlElements.container, enemyType, enemySpeed, enemyLives)
+    #createNewEnemy(enemyType, enemyLives, enemySpeed, explosionClass){
+        const enemy = new Enemy(this.#htmlElements.container, enemyType, enemySpeed, enemyLives, explosionClass)
         enemy.init()
         this.#enemies.push(enemy)
     }
 
     #checkPosition(){
-        this.#ship.missiles.forEach((missile, missileIndex, missileArr) => {
-            
-            const missilePosition = {
-                top: missile.element.offsetTop,
-                right: missile.element.offsetLeft + missile.element.offsetWidth,
-                bottom: missile.element.offsetTop + missile.element.offsetHeight,
-                left: missile.element.offsetLeft,
-            }
-
-            if(missilePosition.bottom < 0){
-                missile.remove()
-                missileArr.splice(missileIndex, 1)
-            }
-        })
-
         this.#enemies.forEach((enemy, enemyIndex, enemiesArr) => {
             
             const enemyPosition = {
@@ -63,10 +55,55 @@ class Game{
             }
 
             if(enemyPosition.top > window.innerHeight){
-                enemy.remove()
+                enemy.explode()
                 enemiesArr.splice(enemyIndex, 1)
+                this.#updateLives()
             }
+            
+            this.#ship.missiles.forEach((missile, missileIndex, missileArr) => {
+                
+                const missilePosition = {
+                    top: missile.element.offsetTop,
+                    right: missile.element.offsetLeft + missile.element.offsetWidth,
+                    bottom: missile.element.offsetTop + missile.element.offsetHeight,
+                    left: missile.element.offsetLeft,
+                }
+                
+                if(missilePosition.bottom >= enemyPosition.top && missilePosition.top <= enemyPosition.bottom && missilePosition.right >= enemyPosition.left && missilePosition.left <= enemyPosition.right){
+                    enemy.hit()
+                    if(!enemy.lives){
+                        enemiesArr.splice(enemyIndex, 1)
+                    }
+
+                    missile.remove()
+                    missileArr.splice(missileIndex, 1)
+                    this.#updateScore()
+                }
+                
+                if(missilePosition.bottom < 0){
+                    missile.remove()
+                    missileArr.splice(missileIndex, 1)
+                }
+            })
         })
+    }
+
+    #updateScore(){
+        this.#score++
+        this.#updateScoreText()
+    }
+
+    #updateScoreText(){
+        this.#htmlElements.score.textContent = `Score: ${this.#score}`
+    }
+
+    #updateLives(){
+        this.#lives--
+        this.#updateLivesText()
+    }
+
+    #updateLivesText(){
+        this.#htmlElements.score.textContent = `Score: ${this.#lives}`
     }
 }
 
